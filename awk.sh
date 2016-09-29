202797170727df3dac464e9ff4b9b1c5923d209b
Matthew Smith <matthewsmith4@mail.weber.edu>
2016-09-29 12:50:50 -0600

    awk script displays presidents after 1900

---
+++ awk.sh
@@ -0,0 +1,18 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: awk.sh
+# 
+#         USAGE: ./awk.sh 
+# 
+#   DESCRIPTION: 
+# 
+#        AUTHOR: Matthew Smith (), matthewsmith4@mail.weber.edu
+#  ORGANIZATION: 
+#       CREATED: 09/29/2016 12:48
+#      REVISION:  ---
+#===============================================================================
+
+#set -o nounset                              # Treat unset variables as an error
+
+
